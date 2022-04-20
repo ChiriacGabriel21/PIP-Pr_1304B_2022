7a8251cefa06e0962cb3d2af327b4f7d77a5bb72
Chiriac <chiriac.gabriel@ac.tuiasi.ro>
2022-04-20 08:35:39 +0300

    Sapt9

--- src/pr_pip/CropButton.java
+++ src/pr_pip/CropButton.java
@@ -1,42 +1,14 @@
 package pr_pip;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.io.File;
+import javax.swing.JButton;
 
 public class CropButton {
-
-    private String[] choiceList = {"Car","Traffic Light","Road Sign","Building","+ADD Option"};
-    public JComboBox chooseFromList;
-
+    public JButton btnCrop;
 
     public void cropButtonInit(DesktopPaneLeft dsktPane){
-        chooseFromList = new JComboBox(choiceList);
-        chooseFromList.setSelectedIndex(-1);
-        chooseFromList.setBounds((dsktPane.desktopPaneLeft.getBounds().width / 2)-30, 100, 120, 30);
-        dsktPane.desktopPaneLeft.add(chooseFromList);
-    }
-
-    public void addIcons(DesktopPaneLeft dsktPane){
-        try {
-            Image cropImage = ImageIO.read(new File("./src/images/test.png")).getScaledInstance(40, 40, Image.SCALE_SMOOTH);
-            ImageIcon cropIcon = new ImageIcon(cropImage);
-
-            Image addImage = ImageIO.read(new File("./src/images/plusSign.png")).getScaledInstance(30, 30, Image.SCALE_SMOOTH);
-            ImageIcon addIcon = new ImageIcon(addImage);
-
-            JLabel cropLabel = new JLabel();
-            cropLabel.setBounds((dsktPane.desktopPaneLeft.getBounds().width / 2) - (120 / 2) - 25, 90, 50, 50);
-            cropLabel.setIcon(cropIcon);
-
-            JLabel addLabel = new JLabel();
-            addLabel.setBounds((dsktPane.desktopPaneLeft.getBounds().width / 2) - (120 / 2) - 17, 12, 50, 50);
-            addLabel.setIcon(addIcon);
-
-            dsktPane.desktopPaneLeft.add(cropLabel);
-            dsktPane.desktopPaneLeft.add(addLabel);
-        }
-        catch(Exception e){}
+    	
+    	btnCrop = new JButton("CROP");
+    	btnCrop.setBounds((dsktPane.desktopPaneLeft.getBounds().width / 2)-30, 180, 120, 30);
+        dsktPane.desktopPaneLeft.add(btnCrop);
     }
 }
--- src/pr_pip/DesktopPaneLeft.java
+++ src/pr_pip/DesktopPaneLeft.java
@@ -1,7 +1,10 @@
 package pr_pip;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
 import java.awt.*;
+import java.io.File;
 
 public class DesktopPaneLeft {
     public JDesktopPane desktopPaneLeft;
@@ -11,5 +14,37 @@ public class DesktopPaneLeft {
         desktopPaneLeft.setBackground(Color.LIGHT_GRAY);
         desktopPaneLeft.setBounds(0, 0, 200, frm.frame.getBounds().height);
         frm.frame.getContentPane().add(desktopPaneLeft);
+    }
+    
+    public void addIcons(){
+        try {
+            Image drawImage = ImageIO.read(new File("./src/images/drawIcon.png")).getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+            ImageIcon drawIcon = new ImageIcon(drawImage);
+
+            Image addImage = ImageIO.read(new File("./src/images/plusSign.png")).getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+            ImageIcon addIcon = new ImageIcon(addImage);
+            
+            Image cropImage = ImageIO.read(new File("./src/images/cropIcon.png")).getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+            ImageIcon cropIcon = new ImageIcon(cropImage);
+
+
+            JLabel drawLabel = new JLabel();
+            drawLabel.setBounds((desktopPaneLeft.getBounds().width / 2) - (120 / 2) - 19, 87, 50, 50);
+            drawLabel.setIcon(drawIcon);
+
+            JLabel addLabel = new JLabel();
+            addLabel.setBounds((desktopPaneLeft.getBounds().width / 2) - (120 / 2) - 17, 12, 50, 50);
+            addLabel.setIcon(addIcon);
+            
+            JLabel cropLabel = new JLabel();
+            cropLabel.setBounds((desktopPaneLeft.getBounds().width / 2) - (120 / 2) - 23, 170, 50, 50);
+            cropLabel.setIcon(cropIcon);
+
+            desktopPaneLeft.add(drawLabel);
+            desktopPaneLeft.add(addLabel);
+            desktopPaneLeft.add(cropLabel);
+            
+        }
+        catch(Exception e){}
     }
 }
